832f53a5f4f5b346ba58c2332f7e5b6d77c8ad89
Mechamen23 <ramirezarboledaa84@gmail.com>
2024-09-23 20:26:41 -0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -39,25 +39,27 @@ fetch('./assets/json/perfumes.json')
                 const tarjeta = document.createElement("div");
                 tarjeta.classList.add("col-md-4");
                 tarjeta.innerHTML = `
-                        <div class="card mb-4 product-wap rounded-0">
-                            <div class="card rounded-0">
-                                <img class="card-img rounded-0 img-fluid" src="./assets/img/perfumes/${perfume.imagen}.png" alt="${perfume.nombre}">
+                        <a href="shop-single.html?id=${perfume.id}">
+                            <div class="card mb-4 product-wap rounded-0">
+                                <div class="card rounded-0">
+                                    <img class="card-img rounded-0 img-fluid" src="./assets/img/perfumes/${perfume.imagen}.png" alt="${perfume.nombre}">
+                                </div>
+                                <div class="card-body">
+                                    <a href="shop-single.html?id=${perfume.id}" class="h3 text-decoration-none">${perfume.nombre}</a>
+                                    <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                        <li>${perfume.casa}</li>
+                                        <li class="pt-2">
+                                            <span class="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
+                                            <span class="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
+                                            <span class="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
+                                            <span class="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
+                                            <span class="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
+                                        </li>
+                                    </ul>
+                                <p class="text-center mb-0">$${perfume.precio}</p>
+                                </div>
                             </div>
-                            <div class="card-body">
-                                <a href="shop-single.html?id=${perfume.id}" class="h3 text-decoration-none">${perfume.nombre}</a>
-                                <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
-                                    <li>${perfume.casa}</li>
-                                    <li class="pt-2">
-                                        <span class="product-color-dot color-dot-red float-left rounded-circle ml-1"></span>
-                                        <span class="product-color-dot color-dot-blue float-left rounded-circle ml-1"></span>
-                                        <span class="product-color-dot color-dot-black float-left rounded-circle ml-1"></span>
-                                        <span class="product-color-dot color-dot-light float-left rounded-circle ml-1"></span>
-                                        <span class="product-color-dot color-dot-green float-left rounded-circle ml-1"></span>
-                                    </li>
-                                </ul>
-                            <p class="text-center mb-0">$${perfume.precio}</p>
-                            </div>
-                        </div>
+                        </a>
                 `;
                 catalogo.appendChild(tarjeta);
             });
